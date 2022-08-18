3fb68275af158426c07ee8cac1a0fecdeafccb12
Raicuparta <raicuparta@gmail.com>
2022-08-18 13:42:03 +0200

    load icon

--- RaiManager/ViewModels/MainWindowViewModel.cs
+++ RaiManager/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 using ReactiveUI;
 
 namespace RaiManager.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public MainWindowViewModel()
+        {
+            LoadIcon();
+        }
+        
+        private const string iconPath = "./icon.png";
+        private Bitmap? icon;
+
+        public Bitmap? Icon
+        {
+            get => icon;
+            private set => this.RaiseAndSetIfChanged(ref icon, value);
+        }
+        
         private string fileNamesText = "Drop files here";
-        public string Greeting
+        public string FileNamesText
         {
             get => fileNamesText;
             set => this.RaiseAndSetIfChanged(ref fileNamesText, value);
@@ -14,7 +32,18 @@ namespace RaiManager.ViewModels
 
         public void DropFiles(IEnumerable<string> files)
         {
-            Greeting = string.Join(", ", files);
+            FileNamesText = string.Join(", ", files);
+        }
+        
+        private async void LoadIcon()
+        {
+            if (File.Exists(iconPath))
+            {
+                Icon = await Task.Run(() => Bitmap.DecodeToWidth(File.OpenRead(iconPath), 400));
+                FileNamesText += ";icon found";
+            }
+
+            FileNamesText += ";icon not found";
         }
     }
 }
