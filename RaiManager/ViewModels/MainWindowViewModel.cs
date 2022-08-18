3daeae3cc53f4cf2051b6b301c5b42c0df595c1c
Raicuparta <raicuparta@gmail.com>
2022-08-18 13:26:51 +0200

    Initial commit

---
+++ RaiManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ReactiveUI;
+
+namespace RaiManager.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string fileNamesText = "Drop files here";
+        public string Greeting
+        {
+            get => fileNamesText;
+            set => this.RaiseAndSetIfChanged(ref fileNamesText, value);
+        }
+
+        public void DropFiles(IEnumerable<string> files)
+        {
+            Greeting = string.Join(", ", files);
+        }
+    }
+}
---
+++ RaiManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,27 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+using RaiManager.ViewModels;
+
+namespace RaiManager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            AddHandler(DragDrop.DropEvent, Drop);
+        }
+
+        private void Drop(object? sender, DragEventArgs dragEvent)
+        {
+            if (DataContext == null) return;
+
+            var fileNames = dragEvent.Data.GetFileNames();
+
+            if (fileNames == null) return;
+
+            ((MainWindowViewModel) DataContext).DropFiles(fileNames);
+        }
+    }
+}
